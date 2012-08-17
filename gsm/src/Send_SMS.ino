5a3ebc749ae997a9d1aeda3de5c73e015160c706
peko <peko@gasubasu.com>
2012-08-17 21:43:42 +0400

    somejunk

--- gsm/src/Send_SMS.ino
+++ gsm/src/Send_SMS.ino
@@ -16,17 +16,25 @@ void setup() {
     gprsSerial.println("AT+CSCS=?");
     delay(100); // даём время на усваивание команды
     ShowSerialData();
+    // Мegafon 
+    // +79219909090
+    // +79262909090
+    // +CSCA: "+79262909090",145
+
+    // • МТС Москва:
+    //+70957699100 (получение всех типов уведомлений) , 
+    //+70957699101 (получение уведомления если сообщение не доставлено адресату) ,
+    //+70957699102 ( уведомления не приходят)
+    //+70957699800 служебный 
     
+    // +CSCA: "+79168999100",145
+
     gprsSerial.println("AT+CSCA?");
-    delay(100); // даём время на усваивание команды
+    delay(100); 
     ShowSerialData();
      
     gprsSerial.println("AT+CMGF=?");
     delay(100); // даём время на усваивание команды
-    ShowSerialData();
-    
-    gprsSerial.println("AT+CMGL=\"ALL\"");
-    delay(10000); // даём время на усваивание команды
     ShowSerialData();
     
     
@@ -44,20 +52,33 @@ void loop() {
  * Функция отправки SMS-сообщения
  */
 void sendTextMessage() {
-   
-  
+
+   // Устанавливает текстовый режим для SMS-сообщений
+    gprsSerial.print("AT+CMGF=1\r");
+    delay(100); // даём время на усваивание команды
+    // Устанавливаем адресата: телефонный номер в международном формате
+    gprsSerial.println("AT + CMGS = \"+79253200641\"");
+    delay(100);
+    // Пишем текст сообщения
+    gprsSerial.println("ALARM!");
+    delay(100);
+    // Отправляем Ctrl+Z, обозначая, что сообщение готово
+    gprsSerial.println((char)26);
+    
+    return;
+    
     // seems like everything is ok
     Serial.println("SEND");
     gprsSerial.println("AT+CMGF=0");
     delay(1000);
     ShowSerialData();
     
-    gprsSerial.println("AT+CMGW=32");
-    delay(2000);
+    gprsSerial.println("AT+CMGW=25");
+    delay(1000);
     ShowSerialData();
     
-    gprsSerial.print("0011000B919762929090F00008AA120422043504410442002D0054006500730074");
-    delay(2000);
+    gprsSerial.print("0011000B919752230046F10000AA0CC8F71D14969741F977FD07");
+    delay(1000);
     ShowSerialData();
     
     // Отправляем Ctrl+Z, обозначая, что сообщение готово
