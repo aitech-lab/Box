03970e036c7bd3b38ecdc7598668a5d0b8c8f393
peko <peko@gasubasu.com>
2012-08-18 06:00:09 +0400

    Some random texts

--- server/daemon.py
+++ server/daemon.py
@@ -1,13 +1,43 @@
+#!/usr/bin/python
+# coding=utf-8
+
 import bottle
 from bottle import route, post, run, request
+import os,random
 
 bottle.debug(True)
 
+randomMessages = ["space1.txt","space2.txt","space3.txt","ufo.txt"]
+
+def cutRandomLineFromFile(filename):
+
+    
+    lines = open(filename,'r').readlines()
+    i =  random.randint(0, len(lines)-1)
+    line = lines[i]
+    del lines[i]
+    open(filename, 'w').writelines(lines)
+    return line
+
+def getRandomLineFromFile(filename):
+    file = open(filename,'r')
+    #Get the total file size
+    file_size = os.stat(filename)[6]
+    file.seek((file.tell()+random.randint(0,file_size-1))%file_size)
+
+    #dont use the first readline since it may fall in the middle of a line
+    file.readline()
+    #this will return the next (complete) line from the file
+    line = file.readline()
+
+    #here is your random line in the file
+    return line
+    
 @route('/')
 def home():
     try:
-        return "Hello Arduino!"
+        return cutRandomLineFromFile("congratulations.txt")
     except Exception, e:
-        print e
+        return getRandomLineFromFile(random.choice(randomMessages))
 
-run(host='193.232.102.70', port=8515, reloader=True)
+run(host='10.0.0.9', port=8515, reloader=True)
